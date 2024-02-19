bc0f2764ad9d5a4fc609a7e205fde5e162404b16
Gurguii <airangomez@hotmail.com>
2024-02-19 19:27:16 +0000

    +

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if (( "$EUID" != 0 )); then
-	printf "need sudo privileges\n"
+if (( "$EUID" == 0 )); then
+	printf "not running with elevated privileges\n"
 	exit 0
 fi
 
@@ -12,33 +12,52 @@ build_dir="$base_dir/build"
 cmakelists="$base_dir/CMakeLists.txt"
 
 log_stderr="$base_dir/.error.log"
-log_stdout="$base_dir/.out.log"
 
 if [ -e "$log_stderr" ]; then
 	rm "$log_stderr"
 fi
 
-if [ -e "$log_stdout" ]; then
-	rm "$log_stdout"
-fi
-
 args=($@)
 argc=$#
 
-if [ -e "$build_dir" ]; then
-	rm -rf "$build_dir"	
+if ! [ -e "$build_dir" ]; then
+	mkdir "$build_dir"	
+	if (( $? != 0 )); then
+		printf "couldn't create build dir '%s'\n" "$build_dir"
+		exit 1
+	fi
 fi
-
-mkdir "$build_dir"
 
 cd "$build_dir"
 
-printf "[info] starting gpkih setup script - %s\n" "$(date '+%d-%m-%Y at %H:%M')" | tee -a "$log_stdout"
-commands=("cmake .." "make" "make package" "make package_source" "cp gpkih ../gpkih")
+declare -A subopts
+subopts["--create-packs"]="make package"
+subopts["--create-source-packs"]="make package_source"
+
+# Commands to run in order to build the executable
+commands=("cmake .." "make")
+
+for arg in "${args[@]}"; do
+	if [ "$arg" == "-h" ]; then
+		cat << EOF
+
+Usage: setup.sh [optional-subopts]
+
+[optional-subopts]
+  --create-packs         : adds 'make package' to the command list
+  --create-source-packs  : adds 'make package_source' to the command list
+
+EOF
+	exit 0
+fi
+	if [ -n "${subopts[$arg]}" ]; then
+		commands+=("${subopts[$arg]}")
+	fi
+done
 
 for command in "${commands[@]}"; do
 	printf "[running] %s\n" "$command" 
-	$command 1>> "$log_stdout" 2>> "$log_stderr"
+	$command 2>> "$log_stderr"
 	if(( $? != 0 )); then
 		printf "Something failed running '%s', check '%s' for more information" "$command" "$log_stderr"
 		exit $?
@@ -47,4 +66,4 @@ done
 
 printf "[info] gpkih succesfully compiled: %s\n" "$src_dir/src/gpkih"
 printf "[info] installation logs can be found at %s\n" "$log_stdout"
-printf "[info] end of gpkih execution\n" | tee -a "$log_stdout" 
+printf "[info] end of gpkih execution\n" | tee -a "$log_stdout"
